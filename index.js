9015d2f5d4734dceb9c5fb3f3897f0db4c982e4f
Shirley Gaw <sgaw@users.noreply.github.com>
2016-11-05 20:51:55 +0100

    Use query URL and handle params

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var http = require('http');
+var url = require('url');
 var dispatcher = require('httpdispatcher');
-var Baby = require('babyparse');
-var fs = require('fs');
+var ElectoralMap = require('./lib/electoral-map');
 
 const PORT=8080;
+
+if (!String.prototype.formatUnicorn) {
+    String.prototype.formatUnicorn = function() {
+        var str = this.toString();
+        if (!arguments.length)
+            return str;
+        var args = typeof arguments[0],
+            args = (("string" == args || "number" == args) ? arguments : arguments[0]);
+        for (arg in args)
+            str = str.replace(RegExp("\\{" + arg + "\\}", "gi"), args[arg]);
+        return str;
+    }
+}
 
 function handleRequest(request, response){
     try {
@@ -16,28 +29,33 @@ function handleRequest(request, response){
     }
 }
 
-dispatcher.onGet("/", function(req, res) {
+dispatcher.onGet("/query", function(req, res) {
+    var queryObject = url.parse(req.url,true).query;
+    console.log(queryObject['trumpStates']);
+    var electoralMap = new ElectoralMap();
+    var trumpStates = [];
+    if ('trumpStates' in queryObject) {
+      trumpStates = queryObject['trumpStates'].split(',');
+    }
+    var clintonStates = [];
+    if ('clintonStates' in queryObject) {
+      clintonStates = queryObject['clintonStates'].split(',');
+    }
+    trumpStates.map(function(state) {
+      electoralMap.assignTrump(state);
+    });
+    clintonStates.map(function(state) {
+      electoralMap.assignClinton(state);
+    });
+
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('{"result": "Success"}');
+    res.end('{ "key" : "{value}"}'.formatUnicorn({
+      'value': electoralMap.serialize()
+    }));
 });
 
 var server = http.createServer(handleRequest);
 
 server.listen(PORT, function(){
-  var stateMap = {};
-  var content = fs.readFileSync('states.csv', { encoding: 'binary' });
-  var parsed = Baby.parse(content,{
-    step: function(row) {
-      var state = row.data.toString().split(',');
-      if (state.length == 2) {
-        stateMap[state[1]] = state[0];
-      }
-    }
-  });
-  var states = [];
-  for (var abbr in stateMap) {
-    states.push(abbr);
-    console.log("state abbr: %s", abbr);
-  }
   console.log("Server listening on: http://localhost:%s", PORT);
 });
